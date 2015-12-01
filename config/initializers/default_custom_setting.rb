42f2772bdf652cbcdacc0e70a0b757a80f760335
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-12-01 22:22:39 +0900

    skip if table does not exist

--- config/initializers/default_custom_setting.rb
+++ config/initializers/default_custom_setting.rb
@@ -1,8 +1,9 @@
 # you can run `bundle exec rails runner 'Rails.cache.clear'` to clear cache
-
-CustomSetting.save_default('site.display_login_link', '1')
-CustomSetting.save_default('site.only_admin_can_upload', '0')
-CustomSetting.save_default('site.signup_enabled', '1')
-CustomSetting.save_default('custom_content.center_top', '')
-CustomSetting.save_default('custom_content.center_bottom', '')
-CustomSetting.save_default('custom_content.right_top', '')
+if ActiveRecord::Base.connection.table_exists?('settings')
+  CustomSetting.save_default('site.display_login_link', '1')
+  CustomSetting.save_default('site.only_admin_can_upload', '0')
+  CustomSetting.save_default('site.signup_enabled', '1')
+  CustomSetting.save_default('custom_content.center_top', '')
+  CustomSetting.save_default('custom_content.center_bottom', '')
+  CustomSetting.save_default('custom_content.right_top', '')
+end
