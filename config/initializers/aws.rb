5b71aa2da7b97b614a42208b387f0eca30ebcbe9
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-12-03 19:48:05 +0900

    add check

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,5 @@
-if (!ENV['OSS_AWS_ACCESS_ID'].empty? && !ENV['OSS_AWS_SECRET_KEY'].empty?)
+if (ENV.has_key?('OSS_AWS_ACCESS_ID') && !ENV['OSS_AWS_ACCESS_ID'].empty? &&
+    ENV.has_key?('OSS_AWS_SECRET_KEY') && !ENV['OSS_AWS_SECRET_KEY'].empty?) then
   Aws.config.update({
     region: ENV['OSS_REGION'],
     credentials: Aws::Credentials.new(ENV['OSS_AWS_ACCESS_ID'], ENV['OSS_AWS_SECRET_KEY']),
