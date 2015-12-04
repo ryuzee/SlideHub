6553978219a1496a610d15c0e817e83dae33582e
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-12-04 10:32:46 +0900

    remove sample production.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,45 +1,18 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
+set :branch, 'master'
+set :rails_env, 'production'
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+# role :app, %w{vagrant@192.168.1.222}
+# role :web, %w{vagrant@192.168.1.222}
+# role :db,  %w{vagrant@192.168.1.222}
 
+server '192.168.1.222', user: 'vagrant', roles: %w{web app db}, password: 'vagrant'
 
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
+# set :ssh_options, {
+#    keys: [File.expand_path(ENV['OSS_DEPLOY_KEY_PATH'])],
+#    forward_agent: true,
+#    auth_methods: %w(publickey)
+#}
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# And/or per server (overrides global)
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+set :ssh_options, {
+   config: false
+}
