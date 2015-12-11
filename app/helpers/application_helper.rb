99a31cf7d0a844076c773f73c485949fd690fd6e
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-12-12 08:37:46 +0900

    fix transcript links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def js_prefix
+    require 'securerandom'
+    require "digest/md5"
+    prefix = "js" + Digest::MD5.hexdigest(SecureRandom.hex + Time.now.strftime("%Y%m%d%H%M%S"))
+    prefix
+  end
 end
