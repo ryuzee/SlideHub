1e26b8276d405f765994f2270d2b127c1bcd5708
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-12-18 11:34:49 +0900

    enable connectivity from the outside of the box

--- script/develop.sh
+++ script/develop.sh
@@ -11,4 +11,10 @@ sudo chmod 755 /tmp/oss_env.sh
 
 mysql -uroot -p$MYSQL_PASSWORD mysql -e "create database openslideshare default character set 'utf8'"
 mysql -uroot -p$MYSQL_PASSWORD mysql -e "create database test_openslideshare default character set 'utf8'"
+mysql -uroot -p$MYSQL_PASSWORD mysql -e "grant all on *.* to 'root'@'*' identified by 'passw0rd'"
+
+sudo sed -i "s/^bind-address.*//" /etc/mysql/my.cnf
+sudo service mysql restart
+
+
 
