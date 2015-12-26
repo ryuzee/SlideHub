4454d45834cc977d510309d7d24bc326e799d3e6
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-12-27 07:50:30 +0900

    fix asset handling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,8 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += ['sticky-footer-navbar.css']
-#Rails.application.config.assets.precompile += ['jquery-1.10.2.min.js']
 Rails.application.config.assets.precompile += ['jquery.lazyload.js']
 Rails.application.config.assets.precompile += ['jquery.sticky-kit.min.js']
 Rails.application.config.assets.precompile += ['mousetrap.min.js']
@@ -19,6 +17,5 @@ Rails.application.config.assets.precompile += ['js.cookie-2.0.4.min.js']
 Rails.application.config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
 
 NonStupidDigestAssets.whitelist = [/glyphicons-halflings-regular\.(eot|svg|woff|ttf)/]
-#NonStupidDigestAssets.whitelist += ['jquery-1.10.2.min.js']
 NonStupidDigestAssets.whitelist += ['jquery.lazyload.js']
 NonStupidDigestAssets.whitelist += ['mousetrap.min.js']
