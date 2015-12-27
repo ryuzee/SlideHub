484765d6d88f0c76c4dc420ab2605c7a321ebe6c
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-12-28 06:48:44 +0900

    remove js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,12 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += ['jquery.lazyload.js']
-Rails.application.config.assets.precompile += ['jquery.sticky-kit.min.js']
-Rails.application.config.assets.precompile += ['mousetrap.min.js']
-Rails.application.config.assets.precompile += ['js.cookie-2.0.4.min.js']
 Rails.application.config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
 
 NonStupidDigestAssets.whitelist = [/glyphicons-halflings-regular\.(eot|svg|woff|ttf)/]
-NonStupidDigestAssets.whitelist += ['jquery.lazyload.js']
-NonStupidDigestAssets.whitelist += ['mousetrap.min.js']
