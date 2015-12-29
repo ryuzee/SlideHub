cec737bc5833ce919be51851af062d70e755ddb3
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2015-12-29 11:19:09 +0900

    Extract codes inserting initial data from migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if User.count.zero?
+  User.create(
+    email: 'admin@example.com',
+    password: 'passw0rd',
+    password_confirmation: 'passw0rd',
+    display_name: 'admin',
+    biography: 'Administrator',
+    admin: true
+  )
+end
+
+if Category.count.zero?
+  Category.create([
+    { id: 1, name: 'Books' },
+    { id: 2, name: 'Business' },
+    { id: 3, name: 'Design' },
+    { id: 4, name: 'Education' },
+    { id: 5, name: 'Entertainment' },
+    { id: 6, name: 'Finance' },
+    { id: 7, name: 'Games' },
+    { id: 8, name: 'Health' },
+    { id: 9, name: 'How-to & DIY' },
+    { id: 10, name: 'Humor' },
+    { id: 11, name: 'Photos' },
+    { id: 12, name: 'Programming' },
+    { id: 13, name: 'Research' },
+    { id: 14, name: 'Science' },
+    { id: 15, name: 'Technology' },
+    { id: 16, name: 'Travel' },
+  ])
+end
