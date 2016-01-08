7f6e6a4f1e1b6b72d723cec58ad228e013a1f3fa
Ryutaro YOSHIBA <ryuzee@gmail.com>
2016-01-08 09:03:34 +0900

    do not execute when env vars were not set

--- spec/models/storage_spec.rb
+++ spec/models/storage_spec.rb
@@ -4,7 +4,9 @@ describe 'Storage' do
   describe 'initialize storage' do
     storage = Storage.new
     it 'should have more than 1 file' do
-      expect(storage.get_file_list('ossfiles-dev', '').count).to be > 0
+      if ENV.has_key?('OSS_BUCKET_NAME') && ENV.has_key?('OSS_AWS_ACCESS_ID') && ENV.has_key?('OSS_AWS_SECRET_KEY')
+        expect(storage.get_file_list('ossfiles-dev', '').count).to be > 0
+      end
     end
   end
 end
