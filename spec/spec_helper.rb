74b8f1ff92b9eba1bd9074d4ab1a797315fa854b
Ryutaro YOSHIBA <ryuzee@gmail.com>
2016-01-08 09:19:56 +0900

    add coverage_dir for CircleCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require 'simplecov'
 
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
 SimpleCov.start do
     add_filter "/vendor/"
     add_filter "/spec/"
