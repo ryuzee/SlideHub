4a4d96ba577281a8ff1568ace0b1ceeb40c4a657
Ryutaro YOSHIBA <ryuzee@gmail.com>
2016-01-15 11:43:50 +0900

    rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,13 @@ require 'codeclimate-test-reporter'
 
 # save to CircleCI's artifacts directory if we're on CircleCI
 if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
   SimpleCov.coverage_dir(dir)
 end
 
 SimpleCov.start do
-  add_filter "/vendor/"
-  add_filter "/spec/"
+  add_filter '/vendor/'
+  add_filter '/spec/'
 
   formatter SimpleCov::Formatter::MultiFormatter[
     SimpleCov::Formatter::HTMLFormatter,
