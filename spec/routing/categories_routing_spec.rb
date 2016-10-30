10e6b6fd5a48c33408788a202408a479b6c3b6c3
Ryutaro YOSHIBA <ryuzee@gmail.com>
2016-10-31 08:13:42 +0900

    remove empty line

--- spec/routing/categories_routing_spec.rb
+++ spec/routing/categories_routing_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe CategoriesController do
   describe 'routing' do
-
     it 'routes to #category' do
       expect(get('/categories/1')).to route_to(controller: 'categories', action: 'show', id: '1')
     end
