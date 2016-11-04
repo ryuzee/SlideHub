dfb9ec23de60f4752bac885faa4896817c1216fd
Ryutaro YOSHIBA <ryuzee@gmail.com>
2016-11-04 16:36:03 +0900

    nested children

--- app/controllers/admin/custom_contents_controller.rb
+++ app/controllers/admin/custom_contents_controller.rb
@@ -1,13 +1,15 @@
-class Admin::CustomContentsController < Admin::ApplicationController
-  def index
-    @settings = CustomSetting.unscoped.where("var like 'custom_content.%'")
-  end
-
-  def update
-    params.require(:settings).map do |param|
-      data = param.to_unsafe_h
-      CustomSetting[data['var']] = data['value']
+module Admin
+  class CustomContentsController < Admin::BaseController
+    def index
+      @settings = CustomSetting.unscoped.where("var like 'custom_content.%'")
     end
-    redirect_to '/admin/custom_contents/'
+
+    def update
+      params.require(:settings).map do |param|
+        data = param.to_unsafe_h
+        CustomSetting[data['var']] = data['value']
+      end
+      redirect_to '/admin/custom_contents/'
+    end
   end
 end
