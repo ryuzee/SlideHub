95bfe401b1c538c867855a392c9d0b2d44052596
Ryutaro YOSHIBA <ryuzee@gmail.com>
2017-03-25 22:04:17 +0900

    create default setting record

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,21 +20,33 @@ end
 
 if Category.count.zero?
   Category.create([
-                    { id: 1, name: 'Books' },
-                    { id: 2, name: 'Business' },
-                    { id: 3, name: 'Design' },
-                    { id: 4, name: 'Education' },
-                    { id: 5, name: 'Entertainment' },
-                    { id: 6, name: 'Finance' },
-                    { id: 7, name: 'Games' },
-                    { id: 8, name: 'Health' },
-                    { id: 9, name: 'How-to & DIY' },
-                    { id: 10, name: 'Humor' },
-                    { id: 11, name: 'Photos' },
-                    { id: 12, name: 'Programming' },
-                    { id: 13, name: 'Research' },
-                    { id: 14, name: 'Science' },
-                    { id: 15, name: 'Technology' },
-                    { id: 16, name: 'Travel' },
-                  ])
+    { id: 1, name: 'Books' },
+    { id: 2, name: 'Business' },
+    { id: 3, name: 'Design' },
+    { id: 4, name: 'Education' },
+    { id: 5, name: 'Entertainment' },
+    { id: 6, name: 'Finance' },
+    { id: 7, name: 'Games' },
+    { id: 8, name: 'Health' },
+    { id: 9, name: 'How-to & DIY' },
+    { id: 10, name: 'Humor' },
+    { id: 11, name: 'Photos' },
+    { id: 12, name: 'Programming' },
+    { id: 13, name: 'Research' },
+    { id: 14, name: 'Science' },
+    { id: 15, name: 'Technology' },
+    { id: 16, name: 'Travel' },
+  ])
+end
+
+if CustomSetting.count.zero?
+  CustomSetting.create([
+    { id: 1, var: 'site.display_login_link', value: '1' },
+    { id: 2, var: 'site.only_admin_can_upload', value: '0' },
+    { id: 3, var: 'site.signup_enabled', value: '1' },
+    { id: 4, var: 'custom_content.center_bottom', value: '' },
+    { id: 5, var: 'custom_content.center_top', value: '' },
+    { id: 6, var: 'custom_content.right_top', value: '' },
+    { id: 7, var: 'site.name', value: 'SlideHub2' }
+  ])
 end
