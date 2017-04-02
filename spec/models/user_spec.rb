dc3e613deb2465a0695d755711306d5e5d3cbef7
Ryutaro YOSHIBA <ryuzee@gmail.com>
2017-04-03 08:08:48 +0900

    add tests to app/model/users.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,25 +35,102 @@ describe 'User' do
   let(:user) { build(:general_user) }
 
   describe 'valid usernames' do
-    valid_usernames = %w(
-      ryuzee
-      ryuzee-1234
-      RYUZEE
-      ryu
-    )
-    it { expect(user).to allow_value(*valid_usernames).for(:username) }
+    it 'can be accepted' do
+      valid_usernames = %w(
+        ryuzee
+        ryuzee-1234
+        ryuzee_1234
+        RYUZEE
+        ryu
+      )
+      expect(user).to allow_value(*valid_usernames).for(:username)
+    end
   end
 
   describe 'invalid usernames' do
-    invalid_usernames = [
-      'ryuzee@example.com',
-      'search',
-      '-ryuzee',
-      'ryu zee',
-      'ry',
-      'ryuzee789012345678901234567890123',
-    ]
-    it { expect(user).not_to allow_value(*invalid_usernames).for(:username) }
+    it 'can not be accepted' do
+      invalid_usernames = [
+        'ryuzee@example.com',
+        'search',
+        '-ryuzee',
+        'ryuzee-',
+        'ryu zee',
+        'ry',
+        'ryuzee789012345678901234567890123',
+        'a#b',
+        'a' * (32 + 1),
+      ]
+      expect(user).not_to allow_value(*invalid_usernames).for(:username)
+    end
+  end
+
+  describe 'invalid display names' do
+    it 'can not be accepted' do
+      invalid_usernames = [
+        'a' * (32 + 1),
+        ' ' * (32 + 1),
+      ]
+      expect(user).not_to allow_value(*invalid_usernames).for(:display_name)
+    end
+  end
+
+  describe 'Creating "User" model' do
+    success_data = { email: 'dummy1@example.com', username: 'dummy1', password: 'dummy12345', display_name: 'dummy1', biography: 'dummy1' }
+    it 'is valid with email, username, password, display_name, biography' do
+      user = User.new(success_data)
+      expect(user.valid?).to eq(true)
+    end
+
+    it 'is invalid without email' do
+      data = success_data.dup
+      data.delete(:email)
+      user = User.new(data)
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'is invalid without password' do
+      data = success_data.dup
+      data.delete(:password)
+      user = User.new(data)
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'is invalid without username' do
+      data = success_data.dup
+      data.delete(:username)
+      user = User.new(data)
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'is invalid without display_name' do
+      data = success_data.dup
+      data.delete(:display_name)
+      user = User.new(data)
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'is invalid without biography' do
+      data = success_data.dup
+      data.delete(:biography)
+      user = User.new(data)
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'is invalid with a username that is already used by others' do
+      general_user = FactoryGirl.create(:general_user)
+      data = success_data.dup
+      data[:username] = general_user[:username]
+      user = User.new(data)
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'is invalid with an email that is already used by others' do
+      general_user = FactoryGirl.create(:general_user)
+      data = success_data.dup
+      data[:email] = general_user[:email]
+      user = User.new(data)
+      expect(user.valid?).to eq(false)
+    end
   end
 
   describe 'username_to_id' do
