5206618bcc6bba71ffbbac77dac0a90d9795441c
Ryutaro YOSHIBA <ryuzee@gmail.com>
2017-04-03 06:23:00 +0900

    add tests to api_v1_slides_controller

--- spec/controllers/api_v1_slides_controller_spec.rb
+++ spec/controllers/api_v1_slides_controller_spec.rb
@@ -8,12 +8,13 @@ RSpec.describe Api::V1::SlidesController, type: :controller do
   list_json_keys += %w(num_of_pages created_at category_name username thumbnail_url transcript_url tags)
 
   describe 'GET #index' do
-    it 'render index' do
+    it 'renders index' do
       create_list(:slide, 2)
       get :index, format: 'json'
       expect(response.status).to eq(200)
       expect(response).to render_template :index
       json = JSON.parse(response.body);
+      expect(json.count).to be > 0
       json.each do |j|
         list_json_keys.each do |k|
           expect(j.key?(k)).to eq(true)
@@ -21,7 +22,7 @@ RSpec.describe Api::V1::SlidesController, type: :controller do
       end
     end
 
-    it 'assigns the @slides' do
+    it 'assigns @slides' do
       @slides = create_list(:slide, 2)
       get :index, format: 'json'
       expect(response.status).to eq(200)
@@ -29,8 +30,40 @@ RSpec.describe Api::V1::SlidesController, type: :controller do
     end
   end
 
+  describe 'GET #index with name' do
+    it 'renders index' do
+      create_list(:slide, 2)
+      get :index, params: { name: 'Hoge' }, format: 'json'
+      expect(response.status).to eq(200)
+      expect(response).to render_template :index
+      json = JSON.parse(response.body);
+      expect(json.count).to be > 0
+      json.each do |j|
+        list_json_keys.each do |k|
+          expect(j.key?(k)).to eq(true)
+        end
+      end
+    end
+  end
+
+  describe 'GET #index with tag' do
+    it 'renders index' do
+      create_list(:slide, 2)
+      get :index, params: { tag: 'Sushi' }, format: 'json'
+      expect(response.status).to eq(200)
+      expect(response).to render_template :index
+      json = JSON.parse(response.body);
+      expect(json.count).to be > 0
+      json.each do |j|
+        list_json_keys.each do |k|
+          expect(j.key?(k)).to eq(true)
+        end
+      end
+    end
+  end
+
   describe 'GET #show' do
-    it 'render show' do
+    it 'renders show' do
       slide = create(:slide)
       get :show, params: { id: slide.id }, format: 'json'
       expect(response.status).to eq(200)
@@ -48,7 +81,7 @@ RSpec.describe Api::V1::SlidesController, type: :controller do
   end
 
   describe 'GET #transcript' do
-    it 'render show' do
+    it 'renders show' do
       slide = create(:slide)
       get :transcript, params: { id: slide.id }, format: 'json'
       expect(response.status).to eq(200)
