976c95c8dbfa2cb89cbb63b93fd74938e88d27b0
Ryutaro YOSHIBA <ryuzee@gmail.com>
2017-10-08 07:34:46 +0900

    rubocop

--- spec/controllers/api/v1/api_v1_users_controller_spec.rb
+++ spec/controllers/api/v1/api_v1_users_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         get 'show', params: { id: '1' }, format: 'json'
         expect(response.status).to eq(200)
         json = JSON.parse(response.body);
-        keys = %w(id username display_name biography slides_count)
+        keys = %w[id username display_name biography slides_count]
         keys.each do |k|
           expect(json.key?(k)).to eq(true)
         end
