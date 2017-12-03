98b6d699ed55b8f29f078a435d95e4e847992366
Ryutaro YOSHIBA <ryuzee@gmail.com>
2017-12-03 17:14:35 +0900

    remove tmp file

--- app/controllers/admin/custom_files_controller.rb
+++ app/controllers/admin/custom_files_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   class CustomFilesController < Admin::BaseController
     before_action :set_custom_file, only: [:destroy]
+    after_action :delete_tmp_file, only: [:create]
 
     def index
       @custom_files = CustomFile.all
@@ -15,17 +16,17 @@ module Admin
       return redirect_to new_admin_custom_file_path if uploaded_file.blank?
 
       filename = uploaded_file.original_filename
-      output_path = Rails.root.join('tmp', uploaded_file.original_filename)
-      File.open(output_path, 'w+b') do |fp|
+      @output_path = Rails.root.join('tmp', uploaded_file.original_filename)
+      File.open(@output_path, 'w+b') do |fp|
         fp.write  uploaded_file.read
       end
 
-      upload_file_list = [output_path]
+      upload_file_list = [@output_path]
       CloudConfig::SERVICE.upload_files(CloudConfig::SERVICE.config.image_bucket_name, upload_file_list, 'custom-files')
 
       existing_record = CustomFile.where(path: filename).first
       if existing_record.nil?
-        @custom_file = CustomFile.create(:path => filename, :description => custom_file_params[:description])
+        @custom_file = CustomFile.create(path: filename, description: custom_file_params[:description])
       else
         @custom_file = existing_record
         @custom_file.description = custom_file_params[:description]
@@ -40,7 +41,10 @@ module Admin
 
     def destroy
       @custom_file.destroy
-      CloudConfig::SERVICE.delete_files(CloudConfig::SERVICE.config.image_bucket_name, [{key: "custom-files/#{@custom_file.path}"}])
+      CloudConfig::SERVICE.delete_files(
+        CloudConfig::SERVICE.config.image_bucket_name,
+        [{ key: "custom-files/#{@custom_file.path}" }],
+      )
       redirect_to admin_custom_files_path
     end
 
@@ -52,6 +56,10 @@ module Admin
 
       def set_custom_file
         @custom_file = CustomFile.find(params[:id])
+      end
+
+      def delete_tmp_file
+        File.delete @output_path if File.exist?(@output_path)
       end
   end
 end
