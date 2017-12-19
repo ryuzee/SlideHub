0a6c8ec4cd2238568a55b963c4ed0c6c07580acb
Ryutaro YOSHIBA <ryuzee@gmail.com>
2017-12-19 22:35:51 +0900

    rubocop

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,14 +1,14 @@
 module SettingsHelper
   def generate_form_field(setting, form)
-    if %w(site.display_login_link site.only_admin_can_upload site.signup_enabled site.header_inverse).include?(setting.var)
-      return form.select :value, [%w(0 0), %w(1 1)], {}, { class: 'form-control' }
+    if %w[site.display_login_link site.only_admin_can_upload site.signup_enabled site.header_inverse].include?(setting.var)
+      return form.select :value, [%w[0 0], %w[1 1]], {}, { class: 'form-control' }
     end
 
-    if %w(site.theme).include?(setting.var)
-      return form.select :value, [%w(default default), %w(dark dark), %w(white white)], {}, { class: 'form-control' }
+    if %w[site.theme].include?(setting.var)
+      return form.select :value, [%w[default default], %w[dark dark], %w[white white]], {}, { class: 'form-control' }
     end
 
-    if %w(site.name site.favicon).include?(setting.var)
+    if %w[site.name site.favicon].include?(setting.var)
       return form.text_field :value, class: 'form-control'
     end
     form.text_area :value, class: 'form-control', size: '30x5'
