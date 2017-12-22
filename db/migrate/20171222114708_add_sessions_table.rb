50610efdae7e792812fdbd493c6eff5f745a429f
Ryutaro YOSHIBA <ryuzee@gmail.com>
2017-12-22 13:07:31 +0900

    reek

--- db/migrate/20171222114708_add_sessions_table.rb
+++ db/migrate/20171222114708_add_sessions_table.rb
@@ -1,9 +1,9 @@
 class AddSessionsTable < ActiveRecord::Migration[5.1]
   def change
-    create_table :sessions do |t|
-      t.string :session_id, null: false
-      t.text :data
-      t.timestamps
+    create_table :sessions do |table|
+      table.string :session_id, null: false
+      table.text :data
+      table.timestamps
     end
 
     add_index :sessions, :session_id, unique: true
