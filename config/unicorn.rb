becf24a3fea64c1d9edc6f59fe5452c0bb6603f0
Ryutaro YOSHIBA <ryuzee@gmail.com>
2018-06-14 21:23:35 +0900

    enable to set timeout value

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
 # From http://qiita.com/Salinger/items/5350b23f8b4e0dcdbe23
 worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
-timeout 15
-preload_app true # 更新時ダウンタイム無し
+timeout Integer(ENV['WEB_TIMEOUT'] || 15)
+preload_app true
 
 listen '/tmp/unicorn.sock'
-# listen "#{rails_root}/tmp/unicorn.sock"
 pid '/tmp/unicorn.pid'
-# pid "#{rails_root}/tmp/unicorn.pid"
 
 before_fork do |_server, _worker|
   ENV['BUNDLE_GEMFILE'] = File.expand_path('Gemfile', ENV['RAILS_ROOT'])
@@ -30,7 +27,6 @@ after_fork do |_server, _worker|
     ActiveRecord::Base.establish_connection
 end
 
-# ログの出力
 unless ENV.key?('RAILS_LOG_TO_STDOUT')
   stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
   stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
