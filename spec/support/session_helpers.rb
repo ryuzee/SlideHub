ea13e3cb13edc1378b7dfe6e7dc4fd7502f9eda6
Ryutaro YOSHIBA <ryuzee@gmail.com>
2018-11-07 21:51:42 +0900

    add signup spec

---
+++ spec/support/session_helpers.rb
@@ -0,0 +1,20 @@
+module SessionHelpers
+
+  def sign_up_with(email, password, confirmation, username, display_name, biography)
+    visit new_user_registration_path
+    fill_in "user_email", with: email
+    fill_in "user_password", with: password
+    fill_in "user_password_confirmation", with: confirmation
+    fill_in "user_username", with: username
+    fill_in "user_display_name", with: display_name
+    fill_in "user_biography", with: biography
+    click_button "Sign up"
+  end
+
+  def sign_in(email, password)
+    visit new_user_session_path
+    fill_in "user_email", with: email
+    fill_in "user_password", with: password
+    click_button "Log in"
+  end
+end
