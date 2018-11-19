33d7bc0ac2c4afde8a5ddae5a4573f889b7da928
Ryutaro YOSHIBA <ryuzee@gmail.com>
2018-11-19 23:54:47 +0900

    remove column

---
+++ db/migrate/20181119145146_remove_paperclip_column.rb
@@ -0,0 +1,8 @@
+class RemovePaperclipColumn < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :avatar_file_name
+    remove_column :users, :avatar_content_type
+    remove_column :users, :avatar_file_size
+    remove_column :users, :avatar_updated_at
+  end
+end
