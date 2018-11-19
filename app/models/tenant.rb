1bf9ea0878038384df4320c8f113aae4d44c9cc7
Ryutaro YOSHIBA <ryuzee@gmail.com>
2018-11-20 08:06:13 +0900

    add validation and tests

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,7 @@
 class Tenant < ApplicationRecord
+  VALID_TENANTNAME_REGEX = /\A[a-zA-Z0-9]{3,32}\z/.freeze
+  validates :name, uniqueness: true,
+                   length: { minimum: 3, maximum: 32 },
+                   format: { with: VALID_TENANTNAME_REGEX },
+                   exclusion: { in: (ReservedWord.list | Subdomain.list) }
 end
--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
-RSpec.describe Tenant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'Tenant' do
+  let(:tenant) { build(:default_tenant) }
+
+  describe 'valid tenantnames' do
+    it 'can be accepted' do
+      valid_tenantnames = %w[
+        ryuzee
+        ryuzee1234
+        RYUZEE
+        ryu
+      ]
+      expect(tenant).to allow_value(*valid_tenantnames).for(:name)
+    end
+  end
+
+  describe 'invalid tenantnames' do
+    it 'can not be accepted' do
+      invalid_tenantnames = [
+        'ryuzee@example.com',
+        '-ryuzee',
+        'ryuzee-',
+        'ryu zee',
+        'ry',
+        'ryuzee789012345678901234567890123',
+        'a#b',
+        'a' * (32 + 1),
+        'admin',
+        'www',
+        'users',
+        'categories',
+        'popular',
+        'latest',
+        'statistics',
+        'dashboard',
+        'dashboards',
+        'api',
+        'www',
+        'blog',
+        'image',
+        'rss',
+      ]
+      expect(tenant).not_to allow_value(*invalid_tenantnames).for(:name)
+    end
+  end
+
+  describe 'Creating "Tenant" model' do
+    success_data = { name: 'dummy1' }
+    it 'is valid with name' do
+      tenant = Tenant.new(success_data)
+      expect(tenant.valid?).to eq(true)
+      expect(tenant.save).to eq(true)
+    end
+
+    it 'is invalid without name' do
+      data = success_data.dup
+      data.delete(:name)
+      tenant = Tenant.new(data)
+      expect(tenant.valid?).to eq(false)
+    end
+
+    it 'is invalid with a tenantname that is already used by others' do
+      default_tenant = FactoryBot.create(:default_tenant)
+      data = success_data.dup
+      data[:name] = default_tenant[:name]
+      tenant = Tenant.new(data)
+      expect(tenant.valid?).to eq(false)
+    end
+  end
 end
