b73077fe7e4e7997f9b13daf9dc576852df19439
Ryutaro YOSHIBA <ryuzee@gmail.com>
2019-06-15 10:02:54 +0900

    rubocop

--- app/helpers/custom_form_error_helper.rb
+++ app/helpers/custom_form_error_helper.rb
@@ -2,13 +2,18 @@ module CustomFormErrorHelper
   def errors(object, attribute)
     error_messages = object.try(:errors).try(:full_messages_for, attribute)
 
-    if error_messages.is_a?(Array)
+    if error_messages.is_a?(Array) && error_messages.count.positive?
       error_contents = create_error_div(error_messages)
     end
-    error_contents || ""
+    error_contents || ''
   end
 
   def create_error_div(error_messages)
-    raw "<small class=""help-block"">#{error_messages.first}</small>"
+    result = <<~HTML
+      <small class="help-block">#{error_messages.first}</small>
+    HTML
+    # rubocop:disable Rails/OutputSafety
+    raw result
+    # rubocop:enable Rails/OutputSafety
   end
 end
