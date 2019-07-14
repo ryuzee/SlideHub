08e97c264c0c4f8752b0b3a4c3993bfb19cf3f47
Ryutaro YOSHIBA <ryuzee@gmail.com>
2019-07-15 08:15:38 +0900

    refactor test

--- spec/models/web_resource_spec.rb
+++ spec/models/web_resource_spec.rb
@@ -4,64 +4,67 @@ describe 'WebResource' do
   # assign concern to a model
   let(:test_class) { Struct.new(:dummy) { include WebResource } }
   let(:dummy) { test_class.new }
+  let(:url_dummy) { 'http://www.example.com/toro.json' }
 
   describe 'get_json' do
     it 'succeed to retrieve json' do
-      stub_request(:any, 'http://www.example.com/toro.json').to_return(
+      stub_request(:any, url_dummy).to_return(
         body: { 'test' => 'ok' }.to_json,
         status: 200,
       )
-      expect(dummy.get_json('http://www.example.com/toro.json', 1)).to eq({ 'test' => 'ok' })
+      expect(dummy.get_json(url_dummy, 1)).to eq({ 'test' => 'ok' })
     end
 
     it 'succeed to retrieve json with redirect' do
-      stub_request(:any, 'http://www.example.com/toro.json').to_return(
+      url_redirect = 'http://www.example.com/uni.json'
+      stub_request(:any, url_dummy).to_return(
         status: 301,
-        headers: { 'Location' => 'http://www.example.com/uni.json' },
+        headers: { 'Location' => url_redirect },
       )
-      stub_request(:any, 'http://www.example.com/uni.json').to_return(
+      stub_request(:any, url_redirect).to_return(
         body: { 'test' => 'ok' }.to_json,
         status: 200,
       )
-      expect(dummy.get_json('http://www.example.com/toro.json', 2)).to eq({ 'test' => 'ok' })
+      expect(dummy.get_json(url_dummy, 2)).to eq({ 'test' => 'ok' })
     end
 
     it 'fail to retrieve json because of 404' do
-      stub_request(:any, 'http://www.example.com/toro.json').to_return(
+      stub_request(:any, url_dummy).to_return(
         status: 404,
       )
-      expect(dummy.get_json('http://www.example.com/toro.json', 1)).to eq(false)
+      expect(dummy.get_json(url_dummy, 1)).to eq(false)
     end
   end
 
   describe 'get_php_serialized_data' do
     it 'return normalized data' do
-      stub_request(:any, 'http://www.example.com/transcript.txt').to_return(
+      stub_request(:any, url_dummy).to_return(
         body: 'a:1:{s:4:"test";s:2:"ok";}',
         status: 200,
       )
-      expect(dummy.get_php_serialized_data('http://www.example.com/transcript.txt', 1)).to eq({ 'test' => 'ok' })
+      expect(dummy.get_php_serialized_data(url_dummy, 1)).to eq({ 'test' => 'ok' })
     end
 
     it 'return empty data if resource was corrupted' do
-      stub_request(:any, 'http://www.example.com/transcript.txt').to_return(
+      stub_request(:any, url_dummy).to_return(
         body: 'abcdefg',
         status: 200,
       )
-      expect(dummy.get_php_serialized_data('http://www.example.com/transcript.txt', 1)).to eq([])
+      expect(dummy.get_php_serialized_data(url_dummy, 1)).to eq([])
     end
 
     it 'return empty data' do
-      stub_request(:any, 'http://www.example.com/transcript.txt').to_return(
+      stub_request(:any, url_dummy).to_return(
         status: 404,
       )
-      expect(dummy.get_php_serialized_data('http://www.example.com/transcript.txt', 1)).to eq([])
+      expect(dummy.get_php_serialized_data(url_dummy, 1)).to eq([])
     end
   end
 
   describe 'get_contents' do
     it 'returns false when url is not valid' do
-      expect(dummy.get_contents('hogehoge://www.example.com/transcript.txt', 1)).to eq(false)
+      url_invalid_protocol = 'hogehoge://www.example.com/transcript.txt'
+      expect(dummy.get_contents(url_invalid_protocol, 1)).to eq(false)
     end
   end
 end
