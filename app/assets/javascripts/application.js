ba8101a1afbe3a57791ec3123eb959e017aefab6
Ryutaro Yoshiba <ryuzee@gmail.com>
2025-01-11 21:40:53 +0900

    remove jquery.equalHeights. add jquery-match-height

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
 //= require rails-ujs
 //= require clipboard/dist/clipboard.min
 //= require jquery-lazyload/jquery.lazyload
-//= require jquery.equalheights/jquery.equalheights.min
+//= require jquery-match-height/dist/jquery.matchHeight
 //= require sticky-kit/dist/sticky-kit.min
 //= require js-cookie/src/js.cookie
 //= require mousetrap/mousetrap.min
