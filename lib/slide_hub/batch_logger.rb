4ebcc37ae57bc4413242e0900e062ae4d9155e39
Ryutaro Yoshiba <ryuzee@gmail.com>
2025-01-13 12:15:20 +0900

    fix logger error

--- lib/slide_hub/batch_logger.rb
+++ lib/slide_hub/batch_logger.rb
@@ -12,18 +12,28 @@ module SlideHub
 
       def logger
         @logger ||= begin
-          logger = ActiveSupport::Logger.new(Rails.root.join('log', "batch_#{Rails.env}.log"), 5, 10 * 1024 * 1024)
-          logger.formatter = Logger::Formatter.new
-          logger.datetime_format = '%Y-%m-%d %H:%M:%S'
+          # メインのログファイル用ロガー
+          file_logger = ActiveSupport::Logger.new(
+            Rails.root.join('log', "batch_#{Rails.env}.log"), 5, 10 * 1024 * 1024
+          )
+          file_logger.formatter = Logger::Formatter.new
+          file_logger.datetime_format = '%Y-%m-%d %H:%M:%S'
 
+          # 標準出力用のロガー
           stdout_logger = ActiveSupport::Logger.new($stdout)
           stdout_logger.formatter = Logger::Formatter.new
           stdout_logger.datetime_format = '%Y-%m-%d %H:%M:%S'
-          multiple_loggers =
-            ActiveSupport::Logger.broadcast(stdout_logger)
-          logger.extend(multiple_loggers)
 
-          logger
+          # ログの複数出力を管理するロガーを作成
+          multi_logger = ActiveSupport::TaggedLogging.new(file_logger)
+          multi_logger.extend(Module.new {
+            define_method(:add) do |severity, message = nil, progname = nil, &block|
+              file_logger.add(severity, message, progname, &block)
+              stdout_logger.add(severity, message, progname, &block)
+            end
+          })
+
+          multi_logger
         end
       end
     end
